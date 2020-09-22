afbae20847b1035689b8c57537d8b6eb5f04c7ba
Kamil Czanik <kamil@czanik.xyz>
2020-09-22 21:43:18 +0200

    Move CalculateDistanceFragment.kt under `xyz.czanik.distanceapp.distance` package

--- app/src/main/java/xyz/czanik/distanceapp/distance/CalculateDistanceFragment.kt
+++ app/src/main/java/xyz/czanik/distanceapp/distance/CalculateDistanceFragment.kt
@@ -1,10 +1,11 @@
-package xyz.czanik.distanceapp
+package xyz.czanik.distanceapp.distance
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import xyz.czanik.distanceapp.R
 
 class CalculateDistanceFragment : Fragment() {
 

9a09e4288244ff001c8e5725d703817e29f00fe2
Kamil Czanik <kamil@czanik.xyz>
2020-09-25 09:45:12 +0200

    Fix rounding distance

--- app/src/main/java/xyz/czanik/distanceapp/util/Math.kt
+++ app/src/main/java/xyz/czanik/distanceapp/util/Math.kt
@@ -1,9 +1,7 @@
 package xyz.czanik.distanceapp.util
 
 import java.math.RoundingMode
-import java.text.DecimalFormat
 
-fun Double.roundOffDecimal(): Double = DecimalFormat("#.##")
-        .also { it.roundingMode = RoundingMode.UP }
-        .format(this)
+fun Double.roundOffDecimal(): Double = toBigDecimal()
+        .setScale(2, RoundingMode.UP)
         .toDouble()
